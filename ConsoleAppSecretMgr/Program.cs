d40d41a490ad4d8da7fafc446d15bc5d65132c68
Arif Wicaksana <arif.wicaksana@nettium.net>
2020-05-11 21:29:04 +0800

    Initial Commit

---
+++ ConsoleAppSecretMgr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+using MongoDB;
+using System.Data.SqlClient;
+
+namespace ConsoleAppSecretMgr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secretManager = new SecretManager();            
+            string secretName = "dev/db/testapp";
+            
+            var cso = JsonConvert.DeserializeObject<SqlServerConnString>(secretManager.Get(secretName));
+            string connString = "Data Source=" + cso.Host + "," + cso.Port + ";Initial Catalog=" + cso.Dbname + ";User ID=" + cso.UserName + ";Password=" + cso.Password;
+            var connection = new SqlConnection(connString);
+
+            string sql = "select count(Username) from dbo.Users";
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+            connection.Open();
+            object count = command.ExecuteScalar();
+            Console.WriteLine("How many user = " + count);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ConsoleAppSecretMgr/SecretManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Amazon.SecretsManager.Model.Internal;
+using Amazon;
+
+
+namespace ConsoleAppSecretMgr
+{   
+
+    public class SecretManager 
+    {
+        public string Get(string secretName)
+        {
+            //var credentials = new StoredProfileAWSCredentials(profileName);
+            var config = new AmazonSecretsManagerConfig { RegionEndpoint = RegionEndpoint.USEast2 };
+            var client = new AmazonSecretsManagerClient(config);
+            //string region = "us-east-2";
+            //IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+            var request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT"
+            };
+
+            GetSecretValueResponse response = null;
+            
+            try
+            {
+                //response = Task.Run(async () => await client.GetSecretValueAsync(request)).Result;
+                response = client.GetSecretValueAsync(request).Result;
+            } 
+            
+            catch (ResourceNotFoundException)
+            {
+                Console.WriteLine("The requested secret " + secretName + " was not found");
+            }
+            catch (InvalidRequestException e)
+            {
+                Console.WriteLine("The request was invalid due to: " + e.Message);
+            }
+            catch (InvalidParameterException e)
+            {
+                Console.WriteLine("The request had invalid params: " + e.Message);
+            }
+
+            return response?.SecretString;
+        }
+    }
+
+}
